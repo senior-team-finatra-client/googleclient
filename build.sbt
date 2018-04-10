b34171c724f6bf9edfe88249500bde77dbff6594
Thomas Oropeza <thomasoropeza@gmail.com>
2018-04-10 11:30:05 -0700

    proper finatra version and finagle client usage

--- build.sbt
+++ build.sbt
@@ -4,11 +4,10 @@ version := "1.0"
 
 scalaVersion := "2.12.2"
 
-val finatraVersion  = "2.11.0"
+val finatraVersion  = "18.3.0"
 
 libraryDependencies ++= Seq(
   "com.twitter" %% "finatra-http" % finatraVersion,
   "com.twitter" %% "finatra-httpclient" % finatraVersion,
   "ch.qos.logback" % "logback-classic" % "1.2.3",
-  "com.iheart" %% "ficus" % "1.4.1"
 )
--- src/main/scala/FitmanClient.scala
+++ src/main/scala/FitmanClient.scala
@@ -1,40 +1,22 @@
-
-import com.twitter.finagle.{Http, Service}
-import com.twitter.finagle.http.{Request, Response}
-import com.twitter.finatra.httpclient.modules.HttpClientModule
-import com.twitter.finatra.httpclient.{HttpClient, RequestBuilder, RichHttpClient}
-import com.twitter.finatra.json.FinatraObjectMapper
-import com.twitter.util.Future
+import com.twitter.finagle.http.Response
+import com.twitter.finagle.{Http, Service, http}
+import com.twitter.util.{Await, Future}
+import com.twitter.conversions.time._
 
 object FitmanClient {
 
   def main(args: Array[String]): Unit = {
 
-    val mapper: FinatraObjectMapper = FinatraObjectMapper.create(injector = null)
-    val path = "/"
-    val req: Request = RequestBuilder.get("google.com:80")
-    val clientModule: FitmanHttpClientModule = new FitmanHttpClientModule()
-
-    // How do we create this service???????
-    val service: Service[Request, Response] = Service.mk[Request, Response] { req: Request =>
-      val rep = Response()
-      rep.statusCode(604)
-      Future.value(rep)
+    //define a client
+    val client: Service[http.Request, http.Response] = Http.client.newService("google.com:80")
+    //define a request
+    val request = http.Request("/")
+    //apply request on the client
+    val future: Future[Response] = client(request)
+    future.onSuccess { response: http.Response =>
+      return response
     }
-
-
-    val client = clientModule.provideHttpClient(mapper, service)
-    client.execute(req).onSuccess{ response: Response =>
-      println("received response " + response.statusCode + " " + response)
-    }
-  }
-
-
-  class FitmanHttpClientModule extends HttpClientModule {
-
-    override def dest: String = "/"
-
-    override def hostname: String = "google.com:80"
-
+    val response: Response = Await.result[Response](future, 2.seconds)
+    println("CODE:" + response.statusCode)
   }
 }
